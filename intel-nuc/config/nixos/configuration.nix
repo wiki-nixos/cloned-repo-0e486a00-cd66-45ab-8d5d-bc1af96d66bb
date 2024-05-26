20cdba19218725d971f6d39b0893832a129a2ae8
user <user@123.de>
2024-05-26 07:19:08 +0000

    cleanup

--- intel-nuc/config/nixos/configuration.nix
+++ intel-nuc/config/nixos/configuration.nix
@@ -17,14 +17,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos-intel-nuc"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos-intel-nuc";
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -81,8 +74,6 @@
   # Configure console keymap
   console.keyMap = "de";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
@@ -128,47 +119,18 @@ services.tailscale.enable = true;
     pkgs.xorg.xmodmap
     pkgs.xorg.xev
 
-    pkgs.mullvad-browser
-
     # pulseaudio volume control
     pkgs.pavucontrol
     pkgs.neofetch
-    # config.nur.repos.mic92.hello-nur
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-	
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
   # enable docker
   virtualisation.docker.enable = true;
-
   # kvm/libvirt etc
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
-
 }
